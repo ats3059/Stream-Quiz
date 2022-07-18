5ab85688728b6a200762b67ca62851211014433f
ats3059 <ats3059@naver.com>
2022-07-18 22:49:34 +0900

    modifyt

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Quiz2 {
 
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
-
+    //toMap 에서 두번째 function은 같은 키일때 새로운 값으로 적용할 것인지 전에 있던 값으로 적용할 것인지 설정하는 것.
     public Map<String, Integer> quiz1() {
         return WORDS.stream()
                 .map(w -> w.substring(0, 1))
